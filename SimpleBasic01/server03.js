20a674a588eb31fdfc9d56d153dda0c5e97447bd
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-22 14:24:29 +0530

    main

--- SimpleBasic01/server03.js
+++ SimpleBasic01/server03.js
@@ -1,5 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
-    res.write("Hello This is write ");
+    // res.write("Hello This is write ");
+    res.write('<h1>Hello this is html tag</h1>')
     res.end("Hello This Is my First Api create");
 }).listen(4500);
