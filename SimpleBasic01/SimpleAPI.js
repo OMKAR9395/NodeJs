a3c3784a18adb16892c6df88a8a3cf4149c54b09
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-22 16:12:13 +0530

    server

--- SimpleBasic01/SimpleAPI.js
+++ SimpleBasic01/SimpleAPI.js
@@ -1,20 +1,46 @@
 const http = require('http');
-let age = 24;
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html");
-    res.write(`
-        <html>
-        <head>
-        <title>Trail And Teach</title>
-        </head>
-        <body>
-        <h1>Hello This First Server Side Programming</h1>
-        <p>Hello THis Is Paragraph</p>
-        <h6>${age}</h6>
-        <h4>${new Date().toDateString()}</h4>
-        </body>
-        </html>
-        `)
-        res.end();
+const { json } = require('stream/consumers');
+const userData = [
+    {
+        name:"omkar",
+        age:24,
+        email:"omkarmohite85@gmail.com",
+        address:"Pune",
+        state:"Maharashtra"
+    },
+    {
+        name:"prakash",
+        age:26,
+        email:"patil@gmail.com",
+        address:"Pune",
+        state:"Tamilnadu"
+    },
+    {
+        name:"mohite",
+        age:20,
+        email:"trailandteach@gmail.com",
+        address:"Pune",
+        state:"goa"
+    },
+    {
+        name:"patil",
+        age:27,
+        email:"patilomkar@gmail.com",
+        address:"Pune",
+        state:"Gujarat"
+    },
+    {
+        name:"omkar",
+        age:24,
+        email:"omkarmohite85@gmail.com",
+        address:"Pune",
+        state:"Odisa"
+    },
+]
+const server = http.createServer((req, resp) => {
+    resp.setHeader("Content-Type", "application/json");
+    resp.write(JSON.stringify(userData));
+    resp.end();
+   
 });
 server.listen(5000);
---
+++ SimpleBasic01/request.js
@@ -0,0 +1,15 @@
+const http = require('http');
+http.createServer((req,resp)=>{
+    if(req.url == '/'){
+        resp.write('<h1>Hello Dashboard page works</h1>')
+    }else if(req.url == '/login'){
+        resp.write('<h1>Hello login page works</h1>')
+    }else if(req.url == '/mainmenu'){
+        resp.write('<h1>Hello Main Menu page works</h1>')
+    }else if(req.url == '/contact'){
+        resp.write('<h1>Hello Contact page works</h1>')
+    }else{
+        resp.write('<h1>Please enter valid url</h1>')
+    }
+    resp.end();
+}).listen(3500);
