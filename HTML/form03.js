7051bdc14e6ea8eaa138260e00e778d1f470cd7c
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-23 20:32:35 +0530

    asynchronous

--- HTML/form03.js
+++ HTML/form03.js
@@ -29,6 +29,17 @@ http.createServer((req,resp)=>{
             console.log(rawData);
             let formData = queryString.parse(rawData);
             console.log(formData);
+
+            //sunchronous function
+            // fs.writeFileSync("./Text/"+formData.name+".txt",rawData);
+
+            //asynchronous function
+            fs.writeFile("./Text/"+formData.name+".txt",rawData,'utf-8',(err)=>{
+                if(err){
+                    resp.end("Internal  server Error");
+                }
+            })
+
             resp.writeHead(200,{"content-type":"text/html"});
             resp.end(`
                 <h2>Form Submitted Successfully</h2>
