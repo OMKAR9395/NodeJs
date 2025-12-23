4729d1d24a27b19a24f5f204b277fcf10e197431
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-23 11:43:48 +0530

    server side html

---
+++ HTML/web.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,resp)=>{
+    fs.readFile('./web.html','utf-8',(err,data)=>{
+        if(err){
+            resp.writeHead(500,{"content-type":"text/html"})
+            resp.write('Internal server error');
+            resp.end();
+            return;
+        }
+        resp.writeHead(200,{"content-type":"text/html"})
+        resp.write(data);
+        resp.end();
+    })
+}).listen(3500);
