6a3f97377e6b3e7a7e75f1c80f65998dc1a17f21
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-25 17:49:25 +0530

    crud

--- SimpleWebsiteUsingNodejs/website.js
+++ SimpleWebsiteUsingNodejs/website.js
@@ -1,5 +1,31 @@
 const http = require('http');
-http.createServer((req,resp)=>{
-    resp.write("Please check response");
-    resp.end();
-}).listen(3800);
+const fs = require('fs');
+
+http.createServer((req, resp) => {
+    if (req.url == '/') {
+        fs.readFile("html/home.html", 'utf-8', (error, data) => {
+            if (error) {
+                resp.writeHead(500, { "content-type": "text/plain" })
+                resp.end("Internal server error");
+                return false;
+            }
+            resp.write(data);
+            resp.end();
+        })
+
+    } else if (req.url == '/style.css') {
+        fs.readFile("html/style.css", 'utf-8', (error, data) => {
+            if (error) {
+                resp.writeHead(500, { "content-type": "text/plain" })
+                resp.end("Css Not Found");
+                return false;
+            }
+             resp.writeHead(200, { "content-type": "text/css" })
+            // resp.write(data);
+            resp.end(data);
+        })
+    }else {
+        resp.writeHead(404, { "Content-Type": "text/plain" });
+        resp.end("Page Not Found");
+    }
+}).listen(4000);
