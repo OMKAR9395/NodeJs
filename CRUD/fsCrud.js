1feeff6907f40e59a794f0795b970aad14a89cd5
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-25 16:26:33 +0530

    crud

--- CRUD/fsCrud.js
+++ CRUD/fsCrud.js
@@ -7,8 +7,18 @@ const fs = require('fs');
 // fs.unlinkSync('files/banana.txt');
 
 //read file   and which format
-const data = fs.readFileSync('files/banana.txt', 'utf-8');
-console.log(data);
+// const data = fs.readFileSync('files/banana.txt', 'utf-8');
+// console.log(data);
 
 //update file
-fs.appendFileSync('files/banana.txt', ' and this is updated files text');
+// fs.appendFileSync('files/banana.txt', ' and this is updated files text');
+
+//terminal input
+// console.log(process);
+const operation = process.argv[2];
+if (operation == 'write') {
+  const name = process.argv[3];
+  const content = process.argv[4];
+  // console.log(operation, name, content);
+  fs.writeFileSync('files/' + name + '.txt', content);
+}
