79c6c466588bb511cd8387f11c71cdec1f71f5fe
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-27 22:32:12 +0530

    form

--- HTML/form.js
+++ HTML/form.js
@@ -1,16 +1,18 @@
 const http = require('http');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{"content-type":"text/html"});
-   if(req.url == '/'){
-     resp.write(`
+http
+  .createServer((req, resp) => {
+    resp.writeHead(200, { 'content-type': 'text/html' });
+    if (req.url == '/') {
+      resp.write(`
         <form action="/submit" method="post">
         <input type="text" placeholder= "Enter Your Name" name="name"/>
         <input type="email" placeholder= "Enter Your Email" name="email"/>
         <button >Click Me</Button>
         </form>
-        `)
-   }else if(req.url == '/submit'){
-    resp.write('<h1>Data Submitted</h1>');
-   }
+        `);
+    } else if (req.url == '/submit') {
+      resp.write('<h1>Data Submitted</h1>');
+    }
     resp.end();
-}).listen(4000);
+  })
+  .listen(4000);
